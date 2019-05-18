9fffdae6c43663e6831913a67054c5a3b308c672
TriciaCX <tricia_chen@foxmail.com>
2019-05-18 10:59:42 +0800

    增加2018年中国研究生数学建模比赛B题题目和论文简介

--- Entropy.m
+++
@@ -1,4 +0,0 @@
-function F = Entropy(p)
-    F = [sum(p.*log2(p))+3;
-        sum(p)-1];
-end
--- Hfun.m
+++
@@ -1,8 +0,0 @@
-function f = Hfun (P)
-global M
-
-H = 0;
-for i = 1 : M
-    H = H + P(i) * log2(P(i));
-end
-f = abs(3 + H);
